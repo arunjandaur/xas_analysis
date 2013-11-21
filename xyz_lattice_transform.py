b80db392ad9dbd23ad377d65386f88f69e773089
arunjandaur <arun.jandaur@gmail.com>
2013-11-21 15:37:35 -0800

    Tested whether xyz to lattice and vice versa yield the same coordinates back. In other words, xyz --> lattice --> xyz yields the same xyz it started with. Also, added some more documentation.

--- xyz_lattice_transform.py
+++ xyz_lattice_transform.py
@@ -1,4 +1,5 @@
 from numpy.linalg import *
+from numpy import *
 from math import sin, cos
 
 def lattice_xyz_matrix(alpha, beta, gamma):
@@ -6,11 +7,11 @@ def lattice_xyz_matrix(alpha, beta, gamma):
 	Returns a transformation matrix that will yield cartesian coordinates when lattice coordinates are RIGHT multiplied by this transformation matrix.
 	Coordinates are row vectors. i.e: [a b c]
 	"""
-	matrix = []
-	matrix[0] = [1, 0, 0]
-	matrix[1] = [cos(alpha), sin(alpha), 0]
-	matrix[2] = [cos(alpha)*cos(beta), cos(alpha)*sin(beta), sin(beta)]
-	return matrix(matrix)
+	matrx = []
+	matrx.append([1, 0, 0])
+	matrx.append([cos(alpha), sin(alpha), 0])
+	matrx.append([cos(alpha)*cos(beta), cos(alpha)*sin(beta), sin(beta)])
+	return matrix(matrx)
 
 def xyz_lattice_matrix(alpha, beta, gamma):
 	"""
@@ -20,8 +21,18 @@ def xyz_lattice_matrix(alpha, beta, gamma):
 	return inv(lattice_xyz_matrix(alpha, beta, gamma))
 
 def lattice_to_xyz(lattice_vectors, alpha, beta, gamma):
+	"""
+	Input:
+		lattice_vectors (numpy matrix): A collection of row vectors in lattice coordinates
+		alpha (float), beta (float), and gamma (float): The lattice angles as floats
+	Output:
+		A collection of row vectors in xyz coordinates as a numpy array
+	"""
 	return inner(lattice_vectors, lattice_xyz_matrix(alpha, beta, gamma)) #Inner product of lattice row vectors and lattice to XYZ transformation matrix
 
 
 def xyz_to_lattice(cartesian_vectors, alpha, beta, gamma):
+	"""
+	Vice versa of lattice_to_xyz
+	"""
 	return inner(cartesian_vectors, xyz_lattice_matrix(alpha, beta, gamma)) #Inner product of cartesian row vectors and XYZ to lattice transformation matrix
